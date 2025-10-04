32fb8899d4c9ba2b2a5159c2ff8b98bf5cb462ca
behzod <behzod.t@payme.uz>
2025-10-04 20:58:53 +0500

    2 problem

--- day-1.js
+++ day-1.js
@@ -12,4 +12,23 @@
  */
 
 
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+// const createCounter = function (n) {
+//     return function () {
+//         return n++
+//     };
+//
+// };
+
+/**
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
+
+
 
