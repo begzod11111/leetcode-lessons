18bc0f1411e35962a5c20925e887b1fb35734c77
behzod <begzodbey0426@gmail.com>
2025-10-05 17:10:31 +0500

    day 2

---
+++ day-2.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+// var filter = function(arr, fn) {
+//     if (!arr.length) return []
+//     const newArr = []
+//     for (let i in arr) {
+//         const res = fn(arr[i], +i)
+//         if (res) newArr.push(arr[i])
+//     }
+//     return newArr
+// };
