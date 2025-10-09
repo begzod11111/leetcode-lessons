aed09c98ad1315e0b30c12a8e477891a3bd21f8b
behzod <begzodbey0426@gmail.com>
2025-10-09 20:40:03 +0500

    2723. Добавьте два обещания

--- day-4.js
+++ day-4.js
@@ -64,3 +64,20 @@
 // console.log(memoizedFn(2, 3)) // 5; // вычисляется
 // console.log(memoizedFn(2, 3)) // 5; // возвращается из кэша
 // console.log(getCashKey({ '0': 1, '1': 2 })) // 3; // возвращается из кэша
+
+
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+const addTwoPromises = async function (promise1, promise2) {
+    const [res1, res2] = await Promise.all([promise1, promise2]);
+
+    return res1 + res2
+};
+
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */
