f224f7872856dcf23f9affcc253ce733446f69d4
begzod <125487812+begzod11111@users.noreply.github.com>
2026-06-09 16:09:45 +0500

    https://www.codewars.com/kata/5264d2b162488dc400000001/train/javascript

--- 6-days-before-the-interview.js
+++ 6-days-before-the-interview.js
@@ -1,11 +1,35 @@
-console.log("1");
 
-setTimeout(() => {
-  console.log("2: timer");
-}, 0);
 
-Promise.resolve().then(() => {
-  console.log("3: promise");
-});
+// https://www.codewars.com/kata/5168bb5dfe9a00b126000018/train/javascript
 
-console.log("4");
+
+// function solution(str){
+//   return str.split("").reduce((acc, curr) => {
+//       return curr + acc ;
+//   }, "")
+//
+// }
+
+
+
+// https://www.codewars.com/kata/5264d2b162488dc400000001/train/javascript
+
+// function solution(str){
+//   return str.split("").reduce((acc, curr) => {
+//       return curr + acc ;
+//   }, "")
+//
+// }
+//
+//
+// function spinWords(string){
+//
+//   const array =  string.split(" ").reduce((acc, curr) => {
+//     if (curr.length >= 5) {
+//       curr = solution(curr)
+//     }
+//     acc.push(curr)
+//     return acc
+//   }, [])
+//     return array.join("")
+// }
