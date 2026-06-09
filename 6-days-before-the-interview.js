3f451598e29161557562d3a8930b035c4b2c4a4e
begzod <125487812+begzod11111@users.noreply.github.com>
2026-06-09 21:00:54 +0500

    https://www.codewars.com/kata/55bf01e5a717a0d57e0000ec/train/javascript

--- 6-days-before-the-interview.js
+++ 6-days-before-the-interview.js
@@ -1,5 +1,3 @@
-
-
 // https://www.codewars.com/kata/5168bb5dfe9a00b126000018/train/javascript
 
 
@@ -9,7 +7,6 @@
 //   }, "")
 //
 // }
-
 
 
 // https://www.codewars.com/kata/5264d2b162488dc400000001/train/javascript
@@ -37,9 +34,28 @@
 
 // https://www.codewars.com/kata/546f922b54af40e1e90001da/train/javascript
 
-function alphabetPosition(text) {
-    return  text.toLowerCase().replace(/[^a-z]/g, "").split("").map(curr => {
-        return curr.charCodeAt(0) - 96
-    }).join(" ")
+// function alphabetPosition(text) {
+//     return  text.toLowerCase().replace(/[^a-z]/g, "").split("").map(curr => {
+//         return curr.charCodeAt(0) - 96
+//     }).join(" ")
+//
+// }
 
-}
+// https://www.codewars.com/kata/55bf01e5a717a0d57e0000ec/train/javascript
+// function persistence(num) {
+//
+//     function recursiveFunc(num, counter) {
+//         const array = num.toString().split("").map(n => +n)
+//         if (array.length === 1) {
+//             return counter
+//         }
+//
+//         return recursiveFunc(array.reduce((acc, cur) => {
+//             return acc * cur
+//         }), counter + 1)
+//     }
+//     return recursiveFunc(num, 0)
+// }
+
+
+console.log(persistence(2334))
